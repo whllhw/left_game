820e5a9adcdbbf5a62f7c7007b543af40d96217f
lhw <990230292@qq.com>
2019-06-13 16:34:35 +0800

    add 绘制循环

--- src/main/view.py
+++ src/main/view.py
@@ -8,11 +8,12 @@ from src.main.main import matrix_iteration
 
 
 class Application(tk.Frame):
-    def __init__(self, master=None, size=(10, 10)):
+    def __init__(self, master=None, init_matrix=None):
         super().__init__(master)
         self.master = master
-        self.size = size
-        self.grid_ids = np.zeros(size)
+        self.size = init_matrix.shape
+        self.matrix = init_matrix
+        self.grid_ids = np.zeros(self.size)
         self.pack()
         self.create_widgets()
 
@@ -35,24 +36,28 @@ class Application(tk.Frame):
 
     def flush(self, mat):
         tmp = matrix_iteration(mat)
-        flush_mat = tmp == mat
-        # mat = tmp
+        flush_mat = tmp != mat
+        mat = tmp
         h, w = self.cnf["height"] / self.size[0], self.cnf["width"] / self.size[1]
         for i in range(self.size[0]):
             for j in range(self.size[1]):
-                color = "black" if mat[j, i] == 1 else "white"
-                # if flush_mat[i, j]:
-                self.cancas.delete(self.grid_ids[i, j])
-                self.grid_ids[i, j] = self.cancas.create_rectangle(
-                    w * i, h * j, w * (i + 1), h * (j + 1), fill=color)
-
+                color = "black" if mat[i, j] == 1 else "white"
+                if flush_mat[i, j]:
+                    self.cancas.delete(self.grid_ids[i, j])
+                    self.grid_ids[i, j] = self.cancas.create_rectangle(
+                        w * i, h * j, w * (i + 1), h * (j + 1), fill=color)
+        self.matrix = mat
         self.cancas.pack()
+
+    def run(self):
+        self.flush(self.matrix)
+        self.master.after(3, self.run)
 
 
 root = tk.Tk()
 root.geometry("600x600")
-app = Application(master=root)
-mat = np.random.randint(0, 2, [10, 10])
-print(mat)
-app.flush(mat)
+mat = np.random.randint(0, 2, [50, 50])
+
+app = Application(master=root, init_matrix=mat)
+app.run()
 app.mainloop()
