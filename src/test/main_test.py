7c400bf32fe03a7708cbd54a679d99a57805516f
lhw <990230292@qq.com>
2019-06-13 14:36:15 +0800

    add algorithm

---
+++ src/test/main_test.py
@@ -0,0 +1,65 @@
+# coding:utf-8
+
+import unittest
+
+import numpy as np
+
+from src.main.main import matrix_iteration
+
+
+class AlgTest(unittest.TestCase):
+
+    def test_matrix_iteration(self):
+        input = np.array([
+            [0, 0, 0],
+            [1, 1, 1],
+            [0, 0, 0]])
+        excepted = np.array([
+            [0, 1, 0],
+            [0, 1, 0],
+            [0, 1, 0]])
+        result = matrix_iteration(input)
+        self.assertTrue((excepted == result).all())
+
+    def test_case(self):
+        input = np.array([
+            [1, 0, 0],
+            [0, 0, 0],
+            [0, 0, 1]])
+        excepted = np.array([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]])
+        result = matrix_iteration(input)
+        self.assertTrue((excepted == result).all())
+
+    def test_case_2(self):
+        input = np.array([
+            [0, 0, 1],
+            [0, 0, 1],
+            [1, 0, 1]])
+        excepted = np.array([
+            [0, 0, 0],
+            [0, 0, 1],
+            [0, 1, 0]])
+        result = matrix_iteration(input)
+        self.assertTrue((excepted == result).all())
+
+    def test_case_3(self):
+        input = np.array([
+            [0, 0, 0, 1],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [1, 0, 0, 1]
+        ])
+        excepted = np.array([
+            [0, 0, 0, 0],
+            [0, 0, 1, 0],
+            [1, 1, 1, 0],
+            [0, 0, 0, 0]
+        ])
+        result = matrix_iteration(input)
+        self.assertTrue((result == excepted).all(), print(excepted, result))
+
+    def print(self, expected, output):
+        return expected + '\n' + output
